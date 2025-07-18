d0f89188454ec7f92a5f48597ad789a8ac60cfa0
Geetanjali Nayak <geetanjali.nayak@geetanjalissvrs-04.successivetech.com>
2025-07-18 17:18:51 +0530

    fixed dist folder

--- dist/src/Day-3/middlewares/customMiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = customMiddleware;
-function customMiddleware(req, res, next) {
-    console.log(`TimeStamp:${new Date().toLocaleString()} , Method: ${req.method}, URL: ${req.originalUrl}`);
-    next();
-}
--- dist/src/Day-3/middlewares/errorHandling.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errMiddleWare = void 0;
-const errMiddleWare = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: "intrenal error" });
-};
-exports.errMiddleWare = errMiddleWare;
